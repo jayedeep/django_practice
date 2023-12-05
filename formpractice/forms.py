821afd80ae47cdfb820f734cd4fd742b0f6520ca
jaydeep <jaydeeptalaviya8@gmail.com>
2023-12-05 21:10:21 +0530

    added validation using extra method

--- formpractice/forms.py
+++ formpractice/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 from formpractice.models import Student
 
+
+def name_validate(value):
+    if not value.isalpha():
+        raise forms.ValidationError("Please Enter a valid name, Dont use Number in Name")
+    return value
+
 class StudentForm(forms.Form):
-    name = forms.CharField(max_length=100)
+
+
+    name = forms.CharField(max_length=100,validators=[name_validate])
     email = forms.EmailField()
     age = forms.DateField(
         widget=forms.DateInput(attrs={'class': 'date-input', 'type': 'date'}),
@@ -15,3 +23,4 @@ class StudentForm(forms.Form):
         super(StudentForm, self).__init__(*args, **kwargs)
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
+
--- formpractice/views.py
+++ formpractice/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from .models import Student
 from .forms import StudentForm
+from django.contrib import messages
+
+
 # Create your views here.
 def home(request):
     form = StudentForm()
@@ -11,11 +14,11 @@ def home(request):
             name = form.cleaned_data['name']
             email = form.cleaned_data['email']
             age = form.cleaned_data['age']
-            print(">>>>name,email,age",name,email,age)
             student = Student.objects.create(name=name, email=email, age=age)
+            messages.success(request, "Student record inserted.")
             return redirect('home')
         else:
-            print(">>>>>errror",form.errors)
+            messages.error(request, "Got error on Create new record.")
     return render(request, 'formpractice/home.html',{'students':students,'form':form})
 
 
@@ -33,12 +36,14 @@ def single_student_update(request,id):
             student.email=form.cleaned_data['email']
             student.age=form.cleaned_data['age']
             student.save()
+            messages.success(request, "Student record updated.")
             return redirect('home')
         else:
-            print(">>>>>errror",form.errors)
+            messages.error(request, "Got error on Update record.")
     return render(request, 'formpractice/single_student_update.html',{'student_form':student_form})
 
 def student_delete(request,id):
     students = Student.objects.filter(id=id)
     students.delete()
+    messages.success(request, "Student record deleted.")
     return redirect('home')
